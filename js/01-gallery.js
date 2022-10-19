d78af75959b1ad247710d3e34714e9ae8c2b3060
NataliiaVlasenko <108278401+NataliiaVlasenko@users.noreply.github.com>
2022-10-19 10:54:00 +0300

    add task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,70 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+// import * as basicLightbox from 'basiclightbox'
 
 console.log(galleryItems);
+
+//1
+const parentRef = document.querySelector(".gallery");
+const markup = createImagesMarkup(galleryItems);
+
+parentRef.insertAdjacentHTML("beforeend", markup);
+
+function createImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    })
+    .join("");
+}
+
+//2
+
+parentRef.addEventListener("click", onPicClick);
+
+
+let LargeImgRef = "";
+
+function onPicClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // console.log(event.target.nodeName, "code below if");
+
+  //3
+  LargeImgRef = event.target.dataset.source;
+
+  const modal = basicLightbox.create(
+    `
+       
+       <img src="${LargeImgRef}"/>
+          `,
+
+    {
+      onShow: () => window.addEventListener("keydown", onEscKeyPress),
+      onClose: () => window.removeEventListener("keydown", onEscKeyPress),
+    }
+  );
+
+  modal.show();
+
+  function onEscKeyPress(evt) {
+    if (evt.code === "Escape") {
+      modal.close();
+    }
+  }
+}
